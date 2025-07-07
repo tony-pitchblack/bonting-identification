ab1b2bab12c022823d983075f0a5390807fb9aff
Anton Chernov <tony.pitchblack@yandex.ru>
2025-07-07 18:53:24 +0300

    fix mindocr demo

--- run_mindocr_demo.sh
+++ run_mindocr_demo.sh
@@ -1,4 +1,4 @@
-python tools/infer/text/predict_system.py --image_dir data/HF_dataset/samples/ear-tags \
+cd mindocr && python tools/infer/text/predict_system.py --image_dir ../data/HF_dataset/samples/ear-tags \
                                           --det_algorithm DB++  \
                                           --rec_algorithm CRNN  \
                                           --visualize_output True
