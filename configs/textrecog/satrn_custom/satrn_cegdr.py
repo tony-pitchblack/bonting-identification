deb2e5c05d61d3db5bad6161a7cf4267a751b5ec
Anton Chernov <tony.pitchblack@yandex.ru>
2025-07-12 15:16:05 +0300

    Add training notebook for recognition for finetuning pretrained models on CEGD-R - rename eval notebook - add dependency for forked mmengine w/ fixed path for ckpt saving when training w/ mmengine

--- configs/textrecog/satrn_custom/satrn_cegdr.py
+++ configs/textrecog/satrn_custom/satrn_cegdr.py
@@ -35,5 +35,46 @@ val_evaluator = dict(
 
 test_evaluator = val_evaluator
 
-# Working directory for outputs
-work_dir = 'work_dirs/satrn_custom_cegdr' 
+# Dataset settings for training
+train_list = [_base_.cegdr_textrecog_train]  # type: ignore[attr-defined]
+
+train_dataset = dict(
+    type='ConcatDataset',
+    datasets=train_list,
+    pipeline=_base_.train_pipeline,  # type: ignore[attr-defined]
+)
+
+# DataLoader for training
+train_dataloader = dict(
+    _delete_=True,
+
+    # batch_size=128,
+    # num_workers=24,
+
+    batch_size=96,
+    num_workers=18,
+
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=train_dataset,
+)
+
+custom_hooks = [
+    dict(
+        type='EarlyStoppingHook',
+        monitor='recog/word_acc', # name of the metric in the val log
+        rule='greater',           # stop when it stops increasing
+        patience=3,               # epochs with no improvement
+        min_delta=0.001)          # minimum change regarded as improvement
+]
+
+work_dir = f'work_dirs/satrn_custom_cegdr'
+
+default_hooks = dict(
+    checkpoint=dict(
+        type='CheckpointHook',
+        interval=1,
+        save_best='recog/word_acc',
+        rule='greater',
+    )
+) 
--- install_mmocr.sh
+++ install_mmocr.sh
@@ -1,16 +1,25 @@
 mim install 'mmcv>=2.1.0,<2.2.0'
 mim install 'mmdet>=3.2.0,<3.4.0'
-mim install 'mmengine>=0.7.0,<1.1.0'
+# mim install 'mmengine>=0.7.0,<1.1.0'
 
-# HACK: clone into a folder with different name to avoid python import conflicts
+###### Install MMEngine ######
+# Install the stable version of mmengine
+# git clone --branch v0.10.7 --depth 1 https://github.com/open-mmlab/mmengine.git mmengine
+# mim install mmengine/
+
+# Install the custom version of mmengine
+git clone https://github.com/tony-pitchblack/mmengine.git mmengine
+mim install mmengine/
+
+###### Install MMOCR ######
+# Install the stable version of mmocr
 git clone https://github.com/open-mmlab/mmocr.git mmocr
-cd mmocr
 
 # install in non-editable mode
-mim install . # install with mim to use .mim/configs/ for pre-defined configs/models/datasets
+mim install mmocr/ # install with mim to use .mim/configs/ for pre-defined configs/models/datasets
 
 # install in editable mode w/ setuptools ≥64 
-# mim install -e . --config-settings editable_mode=compat   
+# mim install -e mmocr/ --config-settings editable_mode=compat   
 
 # install in editable mode w/ setuptools <64 
-# mim install -e . 
+# mim install -e mmocr/ 
